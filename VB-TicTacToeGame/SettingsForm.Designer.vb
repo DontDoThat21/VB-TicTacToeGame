5fd3f86efc5856bcc85ff77de3428e8c66979faf
Tylor Trub <tylortrub@gmail.com>
2019-02-23 11:51:30 -0500

    GUI Changes for settings form. Expected functionality of forms themselves are now there; AI and player # still wip.

--- VB-TicTacToeGame/SettingsForm.Designer.vb
+++ VB-TicTacToeGame/SettingsForm.Designer.vb
@@ -23,69 +23,93 @@ Partial Class SettingsForm
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
-        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.TrackBar1 = New System.Windows.Forms.TrackBar()
         Me.GroupBox1.SuspendLayout()
         CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GroupBox1
         '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton2)
+        Me.GroupBox1.Controls.Add(Me.RadioButton1)
         Me.GroupBox1.Controls.Add(Me.Label1)
         Me.GroupBox1.Controls.Add(Me.TrackBar1)
-        Me.GroupBox1.Controls.Add(Me.CheckBox2)
-        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
         Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
         Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(234, 248)
+        Me.GroupBox1.Size = New System.Drawing.Size(234, 219)
         Me.GroupBox1.TabIndex = 0
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "Settings"
         '
-        'CheckBox1
+        'Label2
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(33, 43)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
-        Me.CheckBox1.TabIndex = 0
-        Me.CheckBox1.Text = "CheckBox1"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label2.Location = New System.Drawing.Point(53, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 20)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Players:"
         '
-        'CheckBox2
+        'RadioButton2
         '
-        Me.CheckBox2.AutoSize = True
-        Me.CheckBox2.Location = New System.Drawing.Point(120, 43)
-        Me.CheckBox2.Name = "CheckBox2"
-        Me.CheckBox2.Size = New System.Drawing.Size(81, 17)
-        Me.CheckBox2.TabIndex = 1
-        Me.CheckBox2.Text = "CheckBox2"
-        Me.CheckBox2.UseVisualStyleBackColor = True
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.RadioButton2.Location = New System.Drawing.Point(129, 61)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(52, 21)
+        Me.RadioButton2.TabIndex = 5
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Two"
+        Me.RadioButton2.UseVisualStyleBackColor = True
         '
-        'TrackBar1
+        'RadioButton1
         '
-        Me.TrackBar1.Location = New System.Drawing.Point(17, 137)
-        Me.TrackBar1.Name = "TrackBar1"
-        Me.TrackBar1.Size = New System.Drawing.Size(201, 45)
-        Me.TrackBar1.TabIndex = 2
-        Me.TrackBar1.TickFrequency = 5
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.25!)
+        Me.RadioButton1.Location = New System.Drawing.Point(129, 38)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(53, 21)
+        Me.RadioButton1.TabIndex = 4
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "One"
+        Me.RadioButton1.UseVisualStyleBackColor = True
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(98, 121)
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.25!)
+        Me.Label1.Location = New System.Drawing.Point(42, 114)
+        Me.Label1.MinimumSize = New System.Drawing.Size(150, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(39, 13)
+        Me.Label1.Size = New System.Drawing.Size(150, 20)
         Me.Label1.TabIndex = 3
-        Me.Label1.Text = "Label1"
+        Me.Label1.Text = "Medium"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'TrackBar1
+        '
+        Me.TrackBar1.LargeChange = 0
+        Me.TrackBar1.Location = New System.Drawing.Point(17, 137)
+        Me.TrackBar1.Name = "TrackBar1"
+        Me.TrackBar1.Size = New System.Drawing.Size(201, 45)
+        Me.TrackBar1.SmallChange = 5
+        Me.TrackBar1.TabIndex = 2
+        Me.TrackBar1.TickFrequency = 5
+        Me.TrackBar1.TickStyle = System.Windows.Forms.TickStyle.TopLeft
+        Me.TrackBar1.Value = 5
         '
         'SettingsForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(260, 275)
+        Me.ClientSize = New System.Drawing.Size(260, 238)
         Me.Controls.Add(Me.GroupBox1)
         Me.Name = "SettingsForm"
         Me.GroupBox1.ResumeLayout(False)
@@ -98,6 +122,7 @@ Partial Class SettingsForm
     Friend WithEvents GroupBox1 As GroupBox
     Friend WithEvents Label1 As Label
     Friend WithEvents TrackBar1 As TrackBar
-    Friend WithEvents CheckBox2 As CheckBox
-    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
 End Class
--- VB-TicTacToeGame/SettingsForm.vb
+++ VB-TicTacToeGame/SettingsForm.vb
@@ -1,3 +1,21 @@
 ﻿Public Class SettingsForm
-
+    Private Sub TrackBar1_Scroll(sender As Object, e As EventArgs) Handles TrackBar1.Scroll
+        Dim tb As TrackBar
+        tb = CType(sender, TrackBar)
+        If tb.Value < 5 And tb.Value <> 0 Then
+            tb.Value = 0
+        ElseIf tb.Value > 5 And tb.Value < 8 Then
+            tb.Value = 5
+        ElseIf tb.Value > 7 And tb.Value < 11 Then
+            tb.Value = 10
+        End If
+        Select Case tb.Value
+            Case 0
+                Label1.Text = "Easy"
+            Case 5
+                Label1.Text = "Medium"
+            Case 10
+                Label1.Text = "Impossible"
+        End Select
+    End Sub
 End Class
